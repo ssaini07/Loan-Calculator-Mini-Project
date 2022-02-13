e3c30b2688bc8cc5ed188e06cca11f5b895027e8
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2022-02-13 12:16:26 +0530

    Added : Learned about prototypal inheritance

--- js/form.js
+++ js/form.js
@@ -1,12 +1,35 @@
-function Person(name, dob) {
-    this.name = name;
-    this.birthday = new Date(dob);
-    this.calculateAge = function() {
-        const diff = Date.now() - this.birthday.getTime();
-        const ageDate = new Date(diff);
-        return Math.abs(ageDate.getUTCFullYear() - 1970);
-    }
+//Person constructor
+function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
 }
 
-const shubham = new Person('Shubham', '03-25-1995');
-console.log(shubham.calculateAge());
+//Greeting
+Person.prototype.greeting = function() {
+    return `Hello there ${this.firstName} ${this.lastName}`;
+}
+
+const person1 = new Person('Shubham', 'Saini');
+//console.log(person1);
+
+//Customer Constructor
+function Customer(firstName, lastName, phone, membership) {
+    Person.call(this, firstName, lastName);
+
+    this.phone = phone;
+    this.membership = membership;
+}
+
+//Inherit the Person prototype method
+Customer.prototype = Object.create(Person.prototype);
+
+//Create Customer
+const customer1 = new Customer('Sonu', 'Saini', '7206348417', 'Platinum');
+
+console.log(customer1);
+
+//Customer greeting
+Customer.prototype.greeting = function() {
+    return `Hello there ${this.firstName} ${this.lastName} Welcome to our company`;
+}
+console.log(customer1.greeting());
